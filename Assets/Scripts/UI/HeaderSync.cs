e0d4b8fffa6d22caffb72dd082fc95e6e6eb40e0
LeeHaii <thangthcskt@gmail.com>
2026-08-14 16:32:58 +0700

    perf: remove per-frame camera and hover allocations

--- Assets/Scripts/UI/HeaderSync.cs
+++ Assets/Scripts/UI/HeaderSync.cs
@@ -1,26 +1,109 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeaderSync : MonoBehaviour
 {
     public RectTransform contentToSyncWith;
-    private RectTransform _rect;
+    [SerializeField] private ScrollRect scrollRect;
 
-    void Start()
+    private RectTransform _rect;
+    private bool isSubscribed;
+
+    private void Awake()
     {
         _rect = GetComponent<RectTransform>();
     }
 
-    void LateUpdate()
+    private void OnEnable()
+    {
+        if (_rect == null)
+        {
+            _rect = GetComponent<RectTransform>();
+        }
+
+        if (scrollRect == null && contentToSyncWith != null)
+        {
+            scrollRect = contentToSyncWith.GetComponentInParent<ScrollRect>();
+        }
+
+        Subscribe();
+        SyncIfChanged();
+    }
+
+    private void OnDisable()
+    {
+        Unsubscribe();
+    }
+
+    private void OnRectTransformDimensionsChange()
+    {
+        SyncIfChanged();
+    }
+
+    private void Subscribe()
+    {
+        if (isSubscribed)
+        {
+            return;
+        }
+
+        if (scrollRect != null)
+        {
+            scrollRect.onValueChanged.AddListener(OnScrollValueChanged);
+        }
+
+        RectTransform.reapplyDrivenProperties += OnReapplyDrivenProperties;
+        isSubscribed = true;
+    }
+
+    private void Unsubscribe()
+    {
+        if (!isSubscribed)
+        {
+            return;
+        }
+
+        if (scrollRect != null)
+        {
+            scrollRect.onValueChanged.RemoveListener(OnScrollValueChanged);
+        }
+
+        RectTransform.reapplyDrivenProperties -= OnReapplyDrivenProperties;
+        isSubscribed = false;
+    }
+
+    private void OnScrollValueChanged(Vector2 _)
+    {
+        SyncIfChanged();
+    }
+
+    private void OnReapplyDrivenProperties(RectTransform drivenTransform)
+    {
+        if (drivenTransform == contentToSyncWith)
+        {
+            SyncIfChanged();
+        }
+    }
+
+    private void SyncIfChanged()
     {
         if (contentToSyncWith != null && _rect != null)
         {
-            Vector2 pos = _rect.anchoredPosition;
-            pos.x = contentToSyncWith.anchoredPosition.x;
-            _rect.anchoredPosition = pos;
+            float targetX = contentToSyncWith.anchoredPosition.x;
+            if (!Mathf.Approximately(_rect.anchoredPosition.x, targetX))
+            {
+                Vector2 position = _rect.anchoredPosition;
+                position.x = targetX;
+                _rect.anchoredPosition = position;
+            }
 
-            Vector2 sd = _rect.sizeDelta;
-            sd.x = contentToSyncWith.rect.width;
-            _rect.sizeDelta = sd;
+            float targetWidth = contentToSyncWith.rect.width;
+            if (!Mathf.Approximately(_rect.sizeDelta.x, targetWidth))
+            {
+                Vector2 sizeDelta = _rect.sizeDelta;
+                sizeDelta.x = targetWidth;
+                _rect.sizeDelta = sizeDelta;
+            }
         }
     }
 }
